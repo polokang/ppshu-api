f70e42c7c4ec0199d7cd92cee54840784dd71429
Hunter Zhou <polokang@163.com>
2019-09-20 13:57:41 +1000

    Delete hello.js

--- hello.js
+++
@@ -1,9 +0,0 @@
-const http = require("http")
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("hello ppshu")
-  })
-  .listen(3002)
-
-console.log("server test")
